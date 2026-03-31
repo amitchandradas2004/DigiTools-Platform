a583b523865bfcaa743a53dd781f5e03c69d6f2d
amitchandradas2004 <amitcdas792@gmail.com>
2026-03-31 22:20:14 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <h2 className="text-8xl bg-amber-500 p-10 font-bold">
+          This is digitools platform
+        </h2>
+      </div>
+    </>
+  );
+}
+
+export default App;
