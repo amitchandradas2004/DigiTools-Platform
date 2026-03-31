318dbab723d2df0269be3ace5ef4606ad2868a30
amitchandradas2004 <amitcdas792@gmail.com>
2026-03-31 22:26:38 +0600

    Update the app.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from "react";
-import "./App.css";
+import React, { useState } from "react";
+import { ToastContainer } from "react-toastify";
+import Footer from "./Components/Layout/Footer";
+import Navbar from "./Components/Layout/Navbar";
+import Products from "./Components/Pages/Products";
+import GetStarted from "./Components/ui/GetStarted";
+import Header from "./Components/ui/Header";
+import Pricing from "./Components/ui/Pricing";
+import Workflow from "./Components/ui/Workflow";
 
+const ProductsData = async () => {
+  const res = await fetch("/ProductsCard.json");
+  return res.json();
+};
+const ProductsPromis = ProductsData();
 function App() {
+  const [SelectCard, setSelectCard] = useState([]);
   return (
     <>
       <div>
-        <h2 className="text-8xl bg-amber-500 p-10 font-bold">
-          This is digitools platform
-        </h2>
+        <Navbar SelectCard={SelectCard} />
+        <Header />
+        <Products
+          ProductsPromis={ProductsPromis}
+          SelectCard={SelectCard}
+          setSelectCard={setSelectCard}
+        />
+        <GetStarted />
+        <Pricing />
+        <Workflow />
+        <Footer />
       </div>
+      <ToastContainer />
     </>
   );
 }
